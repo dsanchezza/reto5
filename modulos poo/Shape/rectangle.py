111060fb1ebe062084918c4b2e7ddf1f5011f2b6
Danna Gabriela Sánchez Zambrano <dsanchezza@unal.edu.co>
2025-06-30 17:09:25 -0500

    Create rectangle.py

---
+++ modulos poo/Shape/rectangle.py
@@ -0,0 +1,16 @@
+from Shape.shape import Shape
+
+class Rectangle(Shape):
+    def __init__(self, vertices, is_regular: bool):
+        super().__init__(vertices, is_regular)
+
+    def compute_area(self):
+        lado1 = self.edges[0].length
+        lado2 = self.edges[1].length
+        return lado1 * lado2
+
+    def compute_perimeter(self):
+        return sum(edge.length for edge in self.edges)
+
+    def compute_inner_angles(self):
+        return [90.0] * 4
